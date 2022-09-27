b91f0f20b1f5689695d2da74fb3a16196fb5e71c
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-09-26 22:21:01 -0300

    fix: mudando else do addId

--- utils/format_tab.py
+++ utils/format_tab.py
@@ -1,5 +1,5 @@
 def addId(menu: list[dict], tab: dict) -> dict:
     return {
-        "id": menu[-1]["id"] + 1 if len(menu) > 0 else len(menu) + 1,
+        "id": menu[-1]["id"] + 1 if len(menu) > 0 else 1,
         **tab
         }
