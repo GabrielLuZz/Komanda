2f0849243424f81fd41694b7f51a0be06ac3b63d
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-09-26 22:55:00 -0300

    colocando print no addId

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from utils.json_handler import write_json
+
+
 if __name__ == "__main__":
-    # Utilize essa área para testes com print
-    ...
+    new_item = {"name": "CHURROS DO M5", "price": 5.0}
+    print(write_json('menu.json', new_item))
--- utils/format_tab.py
+++ utils/format_tab.py
@@ -1,4 +1,6 @@
 def addId(menu: list[dict], tab: dict) -> dict:
+    print(menu)
+    print(tab)
     return {
         "id": menu[-1]["id"] + 1 if len(menu) > 0 else 1,
         **tab
